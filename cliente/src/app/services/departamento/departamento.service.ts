da26666570bb74aff68a2aeea0a4afaeb0a49b33
Luis <luis.fergon.2@educa.jcyl.es>
2025-05-21 12:27:38 +0200

    21/05 EditarDepartamento funcional

--- cliente/src/app/services/departamento/departamento.service.ts
+++ cliente/src/app/services/departamento/departamento.service.ts
@@ -3,31 +3,32 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DepartamentoService {
-  private urlMostrar="http://localhost:8080/departamentos";
-  private urlGetByID="http://localhost:8080/getByID";
-  private urlEditar="http://localhost:8080/editar";
+	private urlMostrar="http://localhost:8080/departamentos";
+	private urlGetByID="http://localhost:8080/getByID";
+	private urlEditar="http://localhost:8080/editar";
 
-  constructor(private http: HttpClient) { };
+	constructor(private http: HttpClient) { };
 
-  getDepartamentos(): Observable<any>{
-    return this.http.get(this.urlMostrar);
-  };
+	getDepartamentos(): Observable<any>{
+		return this.http.get(this.urlMostrar);
+	};
 
-  findDepartamentoByID(): Observable<any>{
-    return this.http.get(this.urlGetByID, { withCredentials:true});
-  };
+	findDepartamentoByID(): Observable<any>{
+		return this.http.get(this.urlGetByID, { withCredentials:true});
+	};
 
-  cambiarDepartamento(data: any){
-    return this.http.post(this.urlEditar, data);
-  }
+	cambiarDepartamento(data: any){
+		return this.http.post(this.urlEditar, data);
+	}
 
-  editarDepartamento(codigo: string, datos: any): Observable<any>{
-    console.log("departamento.service.ts/editarDepartamento()");
-    console.log(codigo);
-    console.log(datos);
-    return this.http.put(this.urlEditar+'/'+codigo, datos);
-  }
+	editarDepartamento(codigo: string, datos: any): Observable<any>{
+		// console.log("departamento.service.ts/editarDepartamento()");
+		// console.log(codigo);
+		// console.log(datos);
+		// return this.http.put(this.urlEditar+'/'+codigo, datos);
+		return this.http.put(this.urlEditar, datos);
+	}
 }
--- server.js
+++ server.js
@@ -10,87 +10,88 @@ const puerto = 8080;
 
 
 app.use(cors({
-    origin: 'http://localhost:4200',
-    credentials: true
+	origin: 'http://localhost:4200',
+	credentials: true
 }));
 app.use('/editar', rutaDepartamentos);
 app.use(express.json());
 // app.use('/departamento', rutaDepartamentos);
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
+	res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.header('Access-Control-Allow-Credentials', 'true');
+	next();
 });
 app.use(sesion({
-    secret: config.secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        secure: false
-    }
+	secret: config.secret,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		secure: false
+	}
 }));
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, config.options)
-    .then(() => {
-        console.log('MongoDB connected');
-    })
-    .catch((err) => {
-        console.error('Error en la conexion', err);
-    })
+	.then(() => {
+		console.log('MongoDB connected');
+	})
+	.catch((err) => {
+		console.error('Error en la conexion', err);
+	})
 ;
 
 app.get('/', (req, res) =>{
-    res.send("<h1>Hello world</h1>");
+	res.send("<h1>Hello world</h1>");
 });
 app.get('/departamentos', async(req, res)=>{
-    try{
-        const departamentos=await Departamento.find();
-        res.json(departamentos);
-    }catch{
-        res.status(500).json({ error: 'Error al obtener departamentos' });
-    }
+	try{
+		const departamentos=await Departamento.find();
+		res.json(departamentos);
+	}catch{
+		res.status(500).json({ error: 'Error al obtener departamentos' });
+	}
 });
 app.get('/getByID', async(req, res)=>{
-    const idDepartamento=req.session.idDepartamento;
-    if (!idDepartamento) return res.status(400).send({message: 'ID no proporcionado get'});
-    try{
-        const departamento=await Departamento.findById(idDepartamento);
-        if (!departamento) return res.status(400).send({message: 'Departamento no encontrado'});
-        res.json(departamento);
-        console.log(idDepartamento);
-        console.log(departamento);
-    }catch(error){
-        res.status(500).send({message: 'Error al obtener datos'});
-    }
+	const idDepartamento=req.session.idDepartamento;
+	if (!idDepartamento) return res.status(400).send({message: 'ID no proporcionado get'});
+	try{
+		const departamento=await Departamento.findById(idDepartamento);
+		if (!departamento) return res.status(400).send({message: 'Departamento no encontrado'});
+		res.json(departamento);
+		// console.log(idDepartamento);
+		// console.log(departamento);
+	}catch(error){
+		res.status(500).send({message: 'Error al obtener datos'});
+	}
 });
 app.post('/getByID', (req, res)=>{
-    const { idDepartamento } = req.body;
-    if (!idDepartamento) return res.status(400).send({message: 'ID no proporcionado post'});
-    req.session.idDepartamento=idDepartamento;
-    res.send({message: 'ID guardado'});
+	const { idDepartamento } = req.body;
+	if (!idDepartamento) return res.status(400).send({message: 'ID no proporcionado post'});
+	req.session.idDepartamento=idDepartamento;
+	res.send({message: 'ID guardado'});
 });
-app.get('/editar', async(req, res)=>{
-    const codigo = req.params.id;
-    const datos = req.body;
-    console.log("\n"+codigo+"\n"+datos);
-    try {
-        const actualizado = await Departamento.findByIdAndUpdate(
-            { codigo: codigo },
-            datos,
-            { new: true }
-        );
-        if (!actualizado) {
-            return res.status(404).send('No se encontró el departamento.');
-        }
-        res.json(actualizado);
-    } catch (error) {
-        res.status(500).send({ mensaje: 'Error actualizando', error });
-    }
+app.put('/editar', async(req, res)=>{
+	const id = req.body._id;
+	const datos = req.body;
+	console.log(id);
+	console.log(datos);
+	try {
+		const actualizado = await Departamento.findByIdAndUpdate(
+			id,
+			datos,
+			{ new: true }
+		);
+		if (!actualizado) {
+			return res.status(404).send('No se encontró el departamento.');
+		}
+		res.json(actualizado);
+	} catch (error) {
+		res.status(500).send({ mensaje: 'Error actualizando', error });
+	}
 });
 
 app.listen(puerto, () =>{
-    console.log('Servidor funcionando en el puerto '+puerto);
+	console.log('Servidor funcionando en el puerto '+puerto);
 });
